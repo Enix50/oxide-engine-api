6213f4c094f61116b8e08896697ee352716c0406
Enix <Enix.T@yandex.ru>
2026-01-15 17:11:01 +0500

    Add update loop

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub trait WorldContext {
     fn log(&self, msg: &str);
 }
 
-pub trait Script {
+pub trait Script: Send {
     fn init(&mut self, ctx: &dyn WorldContext);
+    fn update(&mut self, ctx: &dyn WorldContext, delta: f32);
 }
