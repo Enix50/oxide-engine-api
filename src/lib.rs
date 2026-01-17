f7a683a8f692ed7a60165bed5020cb0f7e22b146
Enix <Enix.T@yandex.ru>
2026-01-17 14:25:02 +0500

    feat: add ObjectRegistry functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+use std::path::PathBuf;
+/// Уникальный идентификатор объекта в сцене.
+/// Гарантированно уникален в пределах одного запуска движка.
+pub type EntityId = u64;
+
 pub trait WorldContext {
     fn log(&self, msg: &str);
+
+    //ObjectRegistry
+    fn create_root(&self, name: String, script_path: Option<PathBuf>) -> EntityId;
+    fn create_child(&self, name: String, script_path: Option<PathBuf>, parent: EntityId) -> Option<EntityId>;
+    
 }
 
 pub trait Script: Send {
