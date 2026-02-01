af5006ec31b4de81ea88653d019b6a1864ca86a5
Enix <Enix.T@yandex.ru>
2026-02-01 19:07:47 +0500

    feat:

--- src/context.rs
+++ src/context.rs
@@ -8,24 +8,24 @@ pub trait Context {
 	/// script_path: путь к файлу скрипта
 	/// parent_object: объект к оторому будет добавлен объект
 	/// Возвращает `None`, если родитель не существует или объект не может быть создан
-	fn create_object(&mut self, object_name: String, script_path: Option<PathBuf>, parent_object: Option<&impl Object>) -> Option<&impl Object>;
+	fn create_object(&mut self, object_name: String, script_path: Option<PathBuf>, parent_object: Option<&dyn Object>) -> Option<&dyn Object>;
 
 	/// Удаляет объект и всё его поддерево
-	fn remove_object(&mut self, object: &impl Object) -> bool;
+	fn remove_object(&mut self, object: &dyn Object) -> bool;
 
 	/// Устанавливает скрипт для объекта
 	/// если script_path: None, скрипт будет удалён
-	fn set_object_script(&mut self, object: &impl Object, script_path: Option<PathBuf>) -> bool;
+	fn set_object_script(&mut self, object: &dyn Object, script_path: Option<PathBuf>) -> bool;
 
 	/// Перемещает объект к новому родителю
 	/// если new_parent: None, объект станет корнем
-	fn move_object_to_parent(&mut self, child_object: &impl Object, new_parent: Option<&impl Object>) -> bool;
+	fn move_object_to_parent(&mut self, child_object: &dyn Object, new_parent: Option<&dyn Object>) -> bool;
 
 	/// Получает объект по пути
-	fn get_object(&self, object_path: &str) -> Option<&impl Object>;
+	fn get_object(&self, object_path: &str) -> Option<&dyn Object>;
 
 	/// Добавляет объекту компонент
-	fn add_component(&mut self, object: &impl Object, component: Box<impl crate::ComponentData>);
+	fn add_component(&mut self, object: &dyn Object, component: Box<dyn crate::ComponentData>);
 
 	/// Логирование (можно вынести в отдельную подсистему позже)
 	fn log(&self, msg: &str);
